c14f56bb14a770ffc3737da73bf57b1a0442c422
khiari sami <samihada.madaraa@gmail.com>
2022-11-07 19:25:46 +0100

    initial commit

--- login_page/src/App.vue
+++ login_page/src/App.vue
@@ -1,72 +1,18 @@
 <template>
-  <v-app>
-    <v-app-bar app color="blue" dark>
-      <v-toolbar-title>Login</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn text rounded>Home</v-btn>
-      <v-btn text rounded>Login</v-btn>
-      <v-btn text rounded>About</v-btn>
-      <v-btn text rounded>Contact Us</v-btn>
-    </v-app-bar>
-    <v-content>
-      <v-card width="500" class="mx-auto mt-9">
-        <v-card-title>Long Area</v-card-title>
-        <v-card-text>
-          <v-text-field label="Username" prepend-icon="mdi-account-circle"></v-text-field>
-          <v-text-field label="Password"
-          prepend-icon="mdi-lock" 
-          :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-          @click:append="showPassword= !showPassword"
-          :type="showPassword ? 'text':'password'"></v-text-field>
-        </v-card-text>
-        <v-divider></v-divider>
-        <v-card-actions>
-          <v-btn color="success" rounded>Register</v-btn>
-          <v-btn color="info" rounded>Login</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-content>
-
-  <v-footer
-    color="primary lighten-1"
-    padless
-  >
-    <v-row
-      justify="center"
-      no-gutters
-    >
-      <v-btn
-        v-for="link in links"
-        :key="link"
-        color="white"
-        text
-        rounded
-        class="my-2"
-      >
-        {{ link }}
-      </v-btn>
-      <v-col
-        class="primary lighten-2 py-4 text-center white--text"
-        cols="12"
-      >
-        {{ new Date().getFullYear() }} — <strong>Welcome to Vuetify</strong>
-      </v-col>
-    </v-row>
-  </v-footer>
-
-  </v-app>
+<v-app>
+  <v-content>
+    <router-view></router-view>
+  </v-content>
+</v-app>
 </template>
 
 <script>
+
 export default {
-data(){
-  return {
-    showPassword: false,
-  }
-},
-}
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
 </script>
-
-<style>
-
-</style>
---
+++ login_page/src/views/HomeView.vue
@@ -0,0 +1,115 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="8">
+            <v-card class="elevation-12">
+              <v-window v-model="step">
+                <v-window-item :value="1">
+                  <v-row>
+                    <v-col cols="12" md="8">
+                      <v-card-text class="mt-12">
+                        <h1 class="text-center display-2 teal--text text--accent-3">Sign in To Our Website</h1>
+                        <div class="text-center mt-4">
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>fab fa-facebook-f</v-icon>
+                          </v-btn>
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>fab fa-google-plus-g</v-icon>
+                          </v-btn>
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>fab fa-linkedin-in</v-icon>
+                          </v-btn>
+                        </div>
+                        <h4 class="text-center mlt-4">Ensure your email for registration</h4>
+                        <v-form>
+                          <v-text-field label="Email" name="Email" prepend-icon="email" type="text"
+                            color="teal accent-3"></v-text-field>
+                          <v-text-field label="Password" id="password" name="Password" prepend-icon="lock"
+                            type="password" color="teal accent-3"></v-text-field>
+                        </v-form>
+                        <h3 class="text-center mt-3">Forget your password</h3>
+                      </v-card-text>
+                      <div class="text-center mt-3">
+                        <v-btn rounded color="teal accent-3" dark>SIGN IN</v-btn>
+                      </div>
+                    </v-col>
+                    <v-col cols="12" md="4" class="teal accent-3">
+                      <v-card-text class="white--text mt-12">
+                        <h1 class="text-center display-1">Hello Friends</h1>
+                        <h5 class="text-center">Enter Your personnel details and join us</h5>
+                      </v-card-text>
+                      <div class="text-center">
+                        <v-btn rounded outlined dark @click="step++">SIGN UP</v-btn>
+                      </div>
+                    </v-col>
+                  </v-row>
+                </v-window-item>
+                <v-window-item :value="2">
+                  <v-row class="fill-height">
+                    <v-col cols="12" md="4" class="teal accent-3">
+                      <v-card-text class="white--text mt-12">
+                        <h1 class="text-center display-1">Welcome Back</h1>
+                        <h5 class="text-center">To Keep Connected With us please login with your personnel info</h5>
+                      </v-card-text>
+                      <div class="text-center">
+                        <v-btn rounded outlined dark @click="step--">SIGN IN</v-btn>
+                      </div>
+                    </v-col>
+                    <v-col cols="12" md="8">
+                      <v-card-text class="mt-12">
+                        <h1 class="text-center display-2 teal--text text--accent-3">Create Account</h1>
+                        <div class="text-center mt-4">
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>fab fa-facebook-f</v-icon>
+                          </v-btn>
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>fab fa-google-plus-g</v-icon>
+                          </v-btn>
+                          <v-btn class="mx-2" fab color="black" outlined>
+                            <v-icon>fab fa-linkedin-in</v-icon>
+                          </v-btn>
+                        </div>
+                        <h4 class="text-center mt-4">Enter Email for registration</h4>
+                        <v-form>
+                          <v-text-field label="Name" name="Name" prepend-icon="person" type="text"
+                            color="teal accent-3"></v-text-field>
+                          <v-text-field label="Email" name="Email" prepend-icon="email" type="text"
+                            color="teal accent-3" required></v-text-field>
+                          <v-text-field label="Password" name="Password" prepend-icon="lock" type="password"
+                            color="teal accent-3"></v-text-field>
+                          <div class="text-center mt-n3">
+                            <v-btn type="submit" rounded color="teal accent-3" dark>SIGN UP</v-btn>
+                          </div>
+                        </v-form>
+                      </v-card-text>
+
+                    </v-col>
+                  </v-row>
+                </v-window-item>
+              </v-window>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  props: {
+    source: String,
+  },
+  data() {
+    return {
+      step: 1,
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
